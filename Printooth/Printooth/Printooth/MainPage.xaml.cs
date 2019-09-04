b674aeb28c9c6503a08851c9375f301c6389d2a4
kadir aygün <kadir2008@hotmail.com>
2019-09-04 17:44:35 +0300

    first

---
+++ Printooth/Printooth/Printooth/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Printooth
+{
+    public partial class MainPage : ContentPage
+    {
+        PrintPageViewModel prinrpage = new PrintPageViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            picker.BindingContext = prinrpage;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await prinrpage.Print();
+        }
+    }
+}
---
+++ Printooth/Printooth/Printooth/Utility/IBlueToothService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printooth.Utility
+{
+    public interface IBlueToothService
+    {
+        IList<string> GetDeviceList();
+        Task Print(string deviceName, string text);
+    }
+}
