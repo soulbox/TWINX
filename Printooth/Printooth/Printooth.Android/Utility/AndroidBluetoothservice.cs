ea8d9e7436e00eb7eb487f6e61bbe1d28c09a674
kadir aygün <kadir2008@hotmail.com>
2019-09-05 10:54:41 +0300

    test is ok

--- Printooth/Printooth/Printooth.Android/Utility/AndroidBluetoothservice.cs
+++ Printooth/Printooth/Printooth.Android/Utility/AndroidBluetoothservice.cs
@@ -16,34 +16,37 @@ using Printooth.Utility;
 [assembly: Xamarin.Forms.Dependency(typeof(AndroidBlueToothService))]
 namespace Printooth.Droid.Utility
 {
-        class AndroidBlueToothService : IBlueToothService
+    class AndroidBlueToothService : IBlueToothService
+    {
+        /// <summary>
+        /// We have to use local device Bluetooth adapter.
+        /// BondedDevices returns BluetoothDevice collection anyway I need to take just device name.
+        /// </summary>
+        /// <returns></returns>
+        public IList<string> GetDeviceList()
         {
-            /// <summary>
-            /// We have to use local device Bluetooth adapter.
-            /// BondedDevices returns BluetoothDevice collection anyway I need to take just device name.
-            /// </summary>
-            /// <returns></returns>
-            public IList<string> GetDeviceList()
+            using (BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter)
             {
-                using (BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter)
-                {
-                    var btdevice = bluetoothAdapter?.BondedDevices.Select(i => i.Name).ToList();
-                    return btdevice;
-                }
+                var btdevice = bluetoothAdapter?.BondedDevices.Select(i => i.Name).ToList();
+                return btdevice;
             }
+        }
 
-            /// <summary>
-            /// We have to use local device Bluetooth adapter.
-            /// We need to find Bluetooth Device with selected device name.
-            /// Now, we use BluetoothSocket class with most common UUID
-            /// Try to connect BluetoothSocket then convert your text-message to bytearray
-            /// Last step write your bytearray by way of bluetoothSocket
-            /// </summary>
-            /// <param name="deviceName">Selected deviceName</param>
-            /// <param name="text">My printed text-message</param>
-            /// <returns></returns>
-            public async Task Print(string deviceName, string text)
+        /// <summary>
+        /// We have to use local device Bluetooth adapter.
+        /// We need to find Bluetooth Device with selected device name.
+        /// Now, we use BluetoothSocket class with most common UUID
+        /// Try to connect BluetoothSocket then convert your text-message to bytearray
+        /// Last step write your bytearray by way of bluetoothSocket
+        /// </summary>
+        /// <param name="deviceName">Selected deviceName</param>
+        /// <param name="text">My printed text-message</param>
+        /// <returns></returns>
+        public async Task Print(string deviceName, string text)
+        {
+            try
             {
+
 
                 using (BluetoothAdapter bluetoothAdapter = BluetoothAdapter.DefaultAdapter)
                 {
@@ -67,6 +70,12 @@ namespace Printooth.Droid.Utility
                     }
                 }
             }
+            catch (Exception exp)
+            {
+
+                throw exp;
+            }
         }
-    
+    }
+
 }
